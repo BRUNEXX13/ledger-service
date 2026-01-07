8b57ed1da9cb046b042d39d285101fef3b4355e3
BRUNEXX13 <brunocontato2012@gmail.com>
2026-01-06 21:05:21 -0300

    up

--- src/main/java/com/bss/LedgerServiceApplication.java
+++ src/main/java/com/bss/LedgerServiceApplication.java
@@ -2,10 +2,8 @@ package com.bss;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bss"})
 public class LedgerServiceApplication {
 
     public static void main(String[] args) {
